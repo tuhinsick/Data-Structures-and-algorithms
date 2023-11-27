e262204638fe4f75a79f1022e3a7b279e7f45fb0
Khalid Hasan Tuhin <2105002@ugrad.cse.buet.ac.bd>
2023-11-28 00:17:22 +0600

    first

--- MST/onlineA1.cpp
+++ MST/onlineA1.cpp
@@ -2,44 +2,43 @@
 using namespace std;
 
 const int N = 1e5+10;
-vector<int> parent(N);
-vector<int> size(N,0);
+vector<int> parent(N),size(N,0);
 
-void make_set(int v){
-    size[v] = 1;
+void make(int v){
     parent[v] = v;
+    size[v] = 1;
 }
 
-int find_set(int a){
-    if(a = parent[a]){
-        return a;
+int find(int v){
+    if(v == parent[v]){
+        return v;
     }
 
-    return parent[a] = find_set(parent[a]);
+    return parent[v] = find(parent[v]);
 }
 
-void union_set(int a, int b){
-    a = find_set(a);
-    b = find_set(b);
-    if(a!=b){
-        if(size[a] < size[b]){
-            swap(a,b);
+
+void union_set(int u, int v){
+    u = find(u);
+    v = find(v);
+    if(u != v){
+        if(size[u] < size[v]){
+            swap(u,v);
         }
 
-        parent[b] = a;
-        size[a]+=size[b];
+        parent[v] = u;
+        size[u] += v;
     }
 }
-
-int main() {
+int main(){
     freopen("in.txt","r",stdin);
     int n,m;
-    vector<pair<int,pair<int,int>>> edges;
     cin>>n>>m;
     for(int i=0;i<n;i++){
-        make_set(i);
+        make(i);
     }
 
+    vector<pair<int,pair<int,int>>> edges;
     for(int i=0;i<m;i++){
         int u,v,wt;
         cin>>u>>v>>wt;
@@ -48,14 +47,16 @@ int main() {
 
     sort(edges.begin(), edges.end());
     int total = 0;
-    for(auto& edge :edges){
+    cout<<"Edges in MST"<<endl;
+    for(auto edge : edges){
         int wt = edge.first;
         int u = edge.second.first;
         int v = edge.second.second;
-        if(find_set(u) != find_set(v)){
+
+        if(find(u) != find(v)){
             union_set(u,v);
+            cout<<"("<<u<<","<<v<<")"<<endl;
             total += wt;
-            cout<<u<<" "<<v<<endl;
         }
     }
 
